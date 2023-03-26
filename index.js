85311a772e04ed9f6819177db215c505fd47ceed
Jsbro98 <jacobstanbrough@gmail.com>
2023-03-25 20:30:35 -0500

    started with subitForm function, so far have first and last name, pass, and email

--- index.js
+++ index.js
@@ -10,6 +10,44 @@ const confirmPass = document.querySelector('#confirm-pass');
 const submitButton = document.querySelector('.create-account-button');
 const form = document.querySelector('.user-form');
 
-submitButton.addEventListener('click', (e) => {
-  form.submit();
-});
+const submitForm = (e) => {
+  const isUserNameValid = checkRequiredMinAndMax(firstName) && checkRequiredMinAndMax(lastName);
+  const isEmailValid = checkTypeMismatch(email);
+  const isPasswordValid = checkRequiredMinAndMax(password);
+
+  if (isUserNameValid && isEmailValid && isPasswordValid) {
+    form.submit();
+  } else {
+    preventSubmission(e);
+  };
+
+  function checkRequiredMinAndMax(elem) {
+
+    if (elem.validity.tooShort) {
+      console.log('A name is too short');
+      return false;
+    } else if (elem.validity.tooLong) {
+      console.log('A name is too long');
+      return false;
+    } else if (elem.validity.valueMissing) {
+      console.log('Please fill out required fields');
+      return false;
+    }
+
+    return true;
+  };
+
+  function preventSubmission(e) {
+    e.preventDefault();
+    window.history.back();
+  };
+
+  function checkTypeMismatch(elem) {
+    if (elem.validity.typeMismatch) {
+      return false;
+    };
+    return true;
+  };
+};
+
+submitButton.addEventListener('click', submitForm);
